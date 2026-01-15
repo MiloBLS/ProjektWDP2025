25e986308bd89d491a40c10bb9519d8c0e5b8f33
MiloBLS004 <m.ostrowski.337@studms.ug.edu.pl>
2026-01-15 17:54:43 +0100

    dodano zmiany

--- src/states/game_state.py
+++ src/states/game_state.py
@@ -9,6 +9,7 @@ class GameState:
         self.player_hp = 20
         self.current_weapon = 0
         self.last_monster_value = 0
+        self.monsters = 0
         self.cards_played_this_turn = 0
         self.deck = Deck(self.game.assets)
         self.deck.shuffle()
@@ -19,6 +20,10 @@ class GameState:
         self.room_cards = []
         self.room_cards_slots = [(c.pos_x, c.pos_y), (c.pos_x + c.gap, c.pos_y), (c.pos_x + (c.gap * 2), c.pos_y), (c.pos_x + (c.gap * 3), c.pos_y)]
         self._refill_room()
+
+        self.discard_pile = []
+        self.weapon = []
+        self.monsters_pile = []
 
     def _refill_room(self):
         self.can_run = True
@@ -54,7 +59,7 @@ class GameState:
 
         
     def handle_events(self, event):
-        if event.type == pygame.KEYDOWN and not self.game.game_over:
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 self.game.current_state = "MENU"
             if event.key == pygame.K_0:                                                                     #loophole for testing
@@ -62,7 +67,7 @@ class GameState:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if self.run_btn_rect.collidepoint(self.mouse_pos) and self.can_run and self.cards_played_this_turn == 0:
                 self.deck.run(self.room_cards)
-                self.room_cards.clear
+                self.room_cards.clear()
                 self._refill_room()
                 self.can_run = False
         self.handle_card_interactions(event)
@@ -75,7 +80,7 @@ class GameState:
 
         for card in self.room_cards:
             card.update()
-        if self.player_hp == 0:
+        if self.player_hp <= 0:
             self.game.game_over = True
 
     def draw(self, screen):
@@ -91,14 +96,21 @@ class GameState:
                  screen.blit(self.game.assets.get_frame("cbgh", 1), self.deck_rect)
             else:
                  screen.blit(self.game.assets.get_frame("cbg", 0), self.deck_rect)
-        else:
-            pass                                                                            #stop
+                                                                                #stop
 
         for card in self.room_cards:
             img = card.get_img()
             screen.blit(img, card.rect)
-            # pygame.draw.rect(screen, (255, 0, 0), card, 1)                                test hitbox kart n
-
+            # pygame.draw.rect(screen, (255, 0, 0), card, 1)                             test hitbox kart n
+        for card in self.discard_pile:
+            img = card.get_img()
+            screen.blit(img, card.rect)
+        for card in self.monsters_pile:
+            img = card.get_img()
+            screen.blit(img, card.rect)
+        for card in self.weapon:
+            img = card.get_img()
+            screen.blit(img, card.rect)
         # Debug info (można usunąć później)
         font = pygame.font.SysFont(None, 24)
         img = font.render(f"HP: {self.player_hp} Weapon: {self.current_weapon} Last: {self.last_monster_value}", True, (255, 255, 255))
@@ -120,20 +132,30 @@ class GameState:
             card_value = clicked_card.get_value() + 2 
             
             if suit == "kier":
-                if self.player_hp + card_value > 20:
-                    card_value = card_value - ((self.player_hp + card_value) - 20)
+                if self.healed == False:
+                    if self.player_hp + card_value > 20:
+                        card_value = card_value - ((self.player_hp + card_value) - 20)
+                    else:
+                        self.player_hp += card_value
+                    self.healed = True
+                    self.discard_pile.append(clicked_card)
+                    for card in self.discard_pile:
+                        card.move_to(int(c.pos_x + (3 * c.gap)), c.bottom_pos_y)
 
             elif suit == "karo":
+                self.weapon.clear()
+                self.monsters_pile.clear()
                 self.current_weapon = card_value
                 self.last_monster_value = 0
+                self.monsters = 0
+                self.weapon.append(clicked_card)
+                for card in self.weapon:
+                    card.move_to(int(c.pos_x), c.bottom_pos_y)
 
             elif suit in ["trefl", "pik"]:
                 damage = 0
                 
-                # Czy walczymy bronią?
-                # Warunek: Mamy broń ORAZ (broń jest świeża LUB potwór jest słabszy/równy ostatniemu)
-                can_use_weapon = (self.current_weapon > 0) and \
-                                 (self.last_monster_value == 0 or card_value <= self.last_monster_value)
+                can_use_weapon = (self.current_weapon > 0) and (self.last_monster_value == 0 or card_value <= self.last_monster_value)
                 
                 if can_use_weapon:
                     damage = card_value - self.current_weapon
@@ -142,8 +164,12 @@ class GameState:
                     self.last_monster_value = card_value
                 else:
                     damage = card_value
+                self.monsters_pile.append(clicked_card)
+                for card in self.monsters_pile:
+                    card.move_to(int(c.pos_x + c.gap + (20 * self.monsters)), c.bottom_pos_y)
                 
                 self.player_hp -= damage
+                self.monsters += 1
 
             self.room_cards.remove(clicked_card)
             self.cards_played_this_turn += 1
